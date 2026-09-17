e52a0fb3889e3055348a261b61265f7f0c090cc6
Ismail Mustefa <ismailnmustefa@gmail.com>
2026-09-16 20:41:40 -0400

    basemap and fundementals of computer cartography

---
+++ index.js
@@ -0,0 +1,6 @@
+let myMap = L.map("map").setView([37.61, -122.011], 10);
+
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	maxZoom: 20
+}).addTo(myMap);
